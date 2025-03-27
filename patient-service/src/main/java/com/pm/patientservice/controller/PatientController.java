f1f1caff174b3b9c92bded9966942f9510827eed
Roushan Kumar <rk04393@gmail.com>
2025-03-27 13:24:31 +0530

    added somecomments only

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -3,7 +3,6 @@ package com.pm.patientservice.controller;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +24,10 @@ public class PatientController {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
     }
+//    Why Use ResponseEntity Instead of Direct List Return?
+//    More Control Over HTTP Responses Allows setting custom status codes (e.g., NO_CONTENT when no data is available).
+//    Consistent API Responses – Ensures uniform response structures across different endpoints.
+//    Setting HTTP Headers – Enables adding custom headers (e.g., security, CORS).
+//    Handling Edge Cases – Can explicitly return NO_CONTENT instead of an empty list.
+//    Better Exception Handling – Helps return meaningful error responses instead of just throwing exceptions.
 }
