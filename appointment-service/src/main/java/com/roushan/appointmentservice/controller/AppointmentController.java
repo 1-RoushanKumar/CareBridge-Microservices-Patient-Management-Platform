a03df07fad312cea29db9f5bd229e470b5d10afb
Roushan Kumar <rk04393@gmail.com>
2025-06-21 18:00:26 +0530

    I dockerized the appointment-service.

--- appointment-service/src/main/java/com/roushan/appointmentservice/controller/AppointmentController.java
+++ appointment-service/src/main/java/com/roushan/appointmentservice/controller/AppointmentController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/appointments")
+@RequestMapping("/appointments")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
